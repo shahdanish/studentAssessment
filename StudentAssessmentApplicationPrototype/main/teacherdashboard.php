8cbb85b8cb35761fa0ebc4d2bb55f23314a9a046
asif_mwi@yahoo.com <asif_mwi@yahoo.com>
2013-10-28 11:37:58 -0700

    backend files deleted

--- StudentAssessmentApplicationPrototype/main/teacherdashboard.php
+++ StudentAssessmentApplicationPrototype/main/teacherdashboard.php
@@ -1,39 +1,59 @@
 <html>
 	<head>
         <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
         <link rel="stylesheet" type="text/css" href="../css/studentform.css" />
         <link rel="stylesheet" type="text/css" href="../css/style.css" />
 		<link rel="stylesheet" type="text/css" href="../css/animate-custom.css" />
-		<script src="../jquery/jquery-1.10.2.js" ></script>
+	
     </head>
  <body>
- 	
-<?php
-	
-
+	<?php
 	include '../class/studentclass.php';
 	$db = new database('localhost', 'studentassessment', 'root', '');
+	if(isset($_POST['teacher_name'])||isset($_POST['teacher_name'])){
 	$tname=$_POST['teacher_name'];
 	$tpass=$_POST['teacher_pass'];
 	$sql = "SELECT * FROM teacherinfo WHERE teacher_name = '$tname' AND teacher_pass = '$tpass' ";
 	$result=$db->query($sql);
 	$row = $db->fetch();
     $tid = $row['tid'];
-	
-	$db->sessionStart($tname,$tid);
+	$db->sessionStart($tname,$tid);	
+	}
 	if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
-	header ("Location: ../teacherlogin.php");
+	header("Location: ../teacherlogin.php");
 	}
 	else{
-		echo "session is active";
-	}?>
+		if(isset($_POST['classtoadd'])){
+		
+		 $class=$_POST['classtoadd'];
+			echo $class;
+		}
+		?>
+		
 	<div id="wrapper">
-				<p class="login button" id="logoutbutton"> 
-               		<input type="button" value="Logout" id="logout" /> 
+				<p class="login button" id="logoutbutt"> 
+               		<input type="button" value="Logout" id="logout" > 
 				</p>
 	</div>
+	<div id="wrapper">
+                            <form action="#" method="post"> 
+                                <h1> Add classes to student sign up </h1> 
+                                <p> 
+                                    <label for="usernamesignup" class="uname"> add classes</label>
+                                    <input id="classtoadd" name="classtoadd" required="required" type="text" placeholder="add in BS-Se-1st format" />
+                                </p>
+                                <p class="signin button"> 
+									<input type="submit" value="addclass" id="submitted"/> 
+								</p>
+                                
+				            </form>
+    </div>
 	
-	
- </body>
+	<a href ="../backend/addclasses.php">go to add some classes</a>
+ 
+		
+		<?php
+	}
+	?>
+	</body>
  </html>
